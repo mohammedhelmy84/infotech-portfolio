8e180f9336f083269569fb3493a3d452ef55b737
amira-eng954 <106436705+AmiraAli123@users.noreply.github.com>
2025-02-14 14:03:32 +0200

    update project

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\SettingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,6 +28,30 @@ Route::get('/{token}/verify',[AuthController::class,'verify']);
 
 // settings
 Route::apiResource('settings', SettingController::class);
+
+
+// Projects_categories 
+Route::get('/Category',[CategoryController::class,'all']);
+Route::get('/Category/{id}',[CategoryController::class,'show']);
+Route::post('/Category/create',[CategoryController::class,'store']);
+Route::put('/Category/update/{id}',[CategoryController::class,'update']);
+Route::delete('/Category/delete/{id}',[CategoryController::class,'delete']);
+
+// Projects
+// كل المشاريع المتاحه والمخفيه
+Route::get('/projects',[ProjectController::class,'all']);
+Route::get('/projects/{id}',[ProjectController::class,'show']);
+Route::post('/projects/create',[ProjectController::class,'store']);
+Route::put('/projects/update/{id}',[ProjectController::class,'update']);
+Route::delete('/projects/delete/{id}',[ProjectController::class,'delete']);
+
+
+// id كل مشايع الموجوده فى قسم معين  ب 
+Route::get('/project_category/{id}',[ProjectController::class,'categorries']);
+// كل المشاريع المتاحه بس المخفيه لااا
+Route::get('/projects_nothidden',[ProjectController::class,'appear']);
+//كل المشاريع المخفيه
+Route::get('/projects_hidden',[ProjectController::class,'hidden']);
 
 
 
