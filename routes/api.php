f4d87172e4d6e8b15de4d683d6b95c331b8e653c
mohammed_helmy <egyman930@gmail.com>
2025-02-17 01:08:44 +0200

    edit-auth

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register',[AuthController::class,'register']);
+Route::group(['middleware'=>'guest'],function(){ 
+    Route::post('/register',[AuthController::class,'register']);
+});
 Route::post('/login',[AuthController::class,'login']);
 Route::get('/{token}/verify',[AuthController::class,'verify']);
 
